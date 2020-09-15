5c3a7a72c6591853814c8cd2b15d7f8505ef9225
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-09-14 18:40:36 -0700

    set up barebones temperature display, removed unnecessary files and adjust index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="temp-display-container">
+          <div className="temp-display">15°C</div>
+        </div>
+        <div className="button-container">
+          <button>+</button>
+          <button>-</button>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
