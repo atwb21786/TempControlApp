5601604b2a6776b3b64ec890f278dc85d0f7441b
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-09-16 08:02:35 -0700

    set initial state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      temperatureValue: 10,
+    }
+
+    
+  }
+
+  setTemperatureValue = () => {
+    console.log("Temperature")
+  }
+
   render() {
     return (
       <div className="app-container">
         <div className="temp-display-container">
-          <div className="temp-display">15°C</div>
+          <div className="temp-display">{this.state.temperatureValue}°C</div>
         </div>
         <div className="button-container">
-          <button>+</button>
+          <button >+</button>
           <button>-</button>
         </div>
 
