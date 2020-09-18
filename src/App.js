58cb2daff1596e16bfabce436a88533a5859e7e1
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-09-17 21:12:50 -0700

    changed color based on state

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import './App.css';
 class App extends React.Component {
   
   state = {
-    temperatureValue: 15
+    temperatureValue: 15,
+    temperatureColor: '',
   }
 
   increaseTemperature = e => {
     this.setState({
       temperatureValue: this.state.temperatureValue + 1,
     })
+    if(this.state.temperatureValue >= 15) {
+      this.setState({
+        temperatureColor: 'hot'
+      })
+    }
   }
 
   decreaseTemperature = e => {
     this.setState({
       temperatureValue: this.state.temperatureValue - 1,
     })
+    if(this.state.temperatureValue < 15) {
+      this.setState({
+        temperatureColor: 'cold'
+      })
+    }
   }
 
   render() {
     return (
       <div className="app-container">
         <div className="temp-display-container">
-          <div className="temp-display">{this.state.temperatureValue}°C</div>
+          <div className={`temp-display ${this.state.temperatureColor}`}>{this.state.temperatureValue}°C</div>
         </div>
         <div className="button-container">
           <button type="submit" onClick={this.increaseTemperature}>+</button>
